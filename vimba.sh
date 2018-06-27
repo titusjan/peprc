9b880fb556bde7d33bd0e4c656c62612b1798376
Pepijn Kenter <kenter@doddel.houthaak>
2018-06-27 10:49:04 +0200

    Added vimba.sh

---
+++ vimba.sh
@@ -0,0 +1,4 @@
+# Use this to use Vimba. Assuming Vimba is installed in /opt/Vimba_2_1
+
+export GENICAM_GENTL64_PATH="/opt/Vimba_2_1/VimbaGigETL/CTI/x86_64bit"
+alias vimbaviewer=/opt/Vimba_2_1/Tools/Viewer/Bin/x86_64bit/VimbaViewer
