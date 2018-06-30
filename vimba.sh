e8d772c5fba1a8e9d4523f93729eb5b4ad135df2
Pepijn Kenter <kenter@doddel.houthaak>
2018-06-30 22:32:51 +0200

    Updated vimba.sh

--- vimba.sh
+++ vimba.sh
@@ -1,4 +1,10 @@
 # Use this to use Vimba. Assuming Vimba is installed in /opt/Vimba_2_1
 
+export IRIS_EXTERNAL_VIMBA=1
+export VIMBA_HOME="/opt/Vimba_2_1/"
 export GENICAM_GENTL64_PATH="/opt/Vimba_2_1/VimbaGigETL/CTI/x86_64bit"
 alias vimbaviewer=/opt/Vimba_2_1/Tools/Viewer/Bin/x86_64bit/VimbaViewer
+
+# export IRIS_GENICAM_GENTL64_PATH="/opt/Vimba_2_1/VimbaGigETL/CTI/x86_64bit"
+# export IRIS_VIMBA_HOME="/opt/Vimba_2_1/"
+
